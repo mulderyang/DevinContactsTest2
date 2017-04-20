c4f5270509b6603a1b56469e9ab1999473f7b5a0
jeny <iblind@naver.com>
2017-04-20 20:42:36 +0900

    Display self number

--- app/src/main/java/betheracer/devincontactstest2/MainActivity.java
+++ app/src/main/java/betheracer/devincontactstest2/MainActivity.java
@@ -1,13 +1,30 @@
 package betheracer.devincontactstest2;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String host_number;
+    String contacts;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        host_number = manager.getLine1Number();
+
+        textView.setText(host_number);
+
+
     }
 }
